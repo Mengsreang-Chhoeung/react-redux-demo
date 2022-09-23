2878b5acc2820a6e1afdd26405ba73d15f709595
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-09-23 21:12:50 +0700

    remove old version and replace with new version of react and redux tool-kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,57 @@
-import React, { useCallback } from 'react';
-import RestaurantPage from './components/Restaurant';
-import {addRestaurant, removeRestaurant} from './redux/RestaurantActionCreator';
-import {Dispatch} from 'redux';
-import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
 
-const App: React.FC = () => {
-
-  const restaurants: readonly IRestaurant[] = useSelector(
-    (state: RestaurantState) => state.restaurants,
-    shallowEqual
-  );
-
-  const dispatch: Dispatch<any> = useDispatch();
-
-  const saveRestaurant = useCallback(
-    (restaurant: IRestaurant) => {
-      dispatch(addRestaurant(restaurant))
-    },
-    [dispatch]
-  );
-
+function App() {
   return (
-    <React.Fragment>
-      {
-        restaurants.map((restaurant: IRestaurant) => (
-          <RestaurantPage
-            key={restaurant.id} 
-            saveRestaurant={saveRestaurant} 
-            removeRestaurant={removeRestaurant} 
-            restaurant={restaurant}
-          />
-        ))
-      }
-    </React.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
   );
 }
 
